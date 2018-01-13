3ce83d852ab5eea325e4a21bdcddeba9136c866a
Hewlette Dibble <hcodydibble@gmail.com>
2018-01-13 13:55:41 -0800

    added contact page and routes for projects/about and changed css

--- application.py
+++ application.py
@@ -5,3 +5,18 @@ app = Flask(__name__)
 @app.route('/')
 def home_view():
     return render_template('home.html')
+
+
+@app.route('/about')
+def about_view():
+    return render_template('about.html')
+
+
+@app.route('/projects')
+def projects_view():
+    return render_template('projects.html')
+
+
+@app.route('/contact')
+def contact_view():
+    return render_template('contact.html')
